509ebc098b956de9dfe1c9a14ebf53c110725989
MingmaTenzing <pasangsherpa689@gmail.com>
2023-01-08 21:30:12 +0800

    tailwind fix probably

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    content: ['./src/**/*.html'],
+    safelist: [
+      'bg-blue-500',
+      'text-center',
+      'hover:opacity-100',
+      // ...
+      'lg:text-right',
+    ]
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
